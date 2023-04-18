bf40a0efdb6817c38e0f0d296224a700b04653d5
Anor0c <94901070+Anor0c@users.noreply.github.com>
2023-04-18 14:26:52 +0200

    AddEnemyScriptableObject

---
+++ RPGProject/Assets/Louis/Scripts/EnemyScrpitable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScrpitable : ScriptableObject
+{
+    public string ennemyName;
+    public float maxHealth;
+}

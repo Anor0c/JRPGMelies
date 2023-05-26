9f9dff162b0936882a118c69909443e9c9019784
Anor0c <94901070+Anor0c@users.noreply.github.com>
2023-05-26 19:35:24 +0200

    WomanPatternStarted

--- RPGProject/Assets/ColliderFlipBoss.cs
+++ RPGProject/Assets/ColliderFlipBoss.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class ColliderFlipBoss : MonoBehaviour
 {
     [SerializeField]SpriteRenderer sprite;
+    [SerializeField] float ecart = 6; 
     public void Flip()
     {
         if (sprite.flipX == true)
         {
-            transform.localPosition = new Vector3(6,0,0);  
+            transform.localPosition = new Vector3(ecart,0,0);  
         }
         else
         {
-            transform.localPosition = new Vector3(-6,0,0);
+            transform.localPosition = new Vector3(-ecart,0,0);
         }
     }
 }
